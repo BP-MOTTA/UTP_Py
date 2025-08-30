9690642f657f28231d5c227446479a9a529ca29e
fati <dispaussable@gmail.com>
2025-08-29 22:11:50 -0500

    Revision de datos

--- s3_separacion.py
+++ s3_separacion.py
@@ -1,19 +1,21 @@
-import random as rd
-Vingreso=[] #lista vacia
-for i in range (15): #incio de un bucle es con el : el identado es importante
-    Vingreso.append(rd.randint(1,30)) #append añade a la lista
-print(Vingreso) 
+#import random as rd
+#Vingreso=[] #lista vacia
+#for i in range (20): #incio de un bucle es con el : el identado es importante
+#    Vingreso.append(rd.randint(1,30)) #append añade a la lista
+#print(Vingreso) 
 
+Vingreso=[20,40,10,15,3,6,12,4] #valores ingresados por archivo, comando o lecturas
 Vmayor=[]
 Vmenor=[]
 for i in Vingreso:
-    if i >= 5:
+    if i >= 15:
         Vmayor.append(i)
     else:
         Vmenor.append(i)
 print(Vmayor)
 print(Vmenor)
 
-#hacer que el codigo genere 20 valores hasta 30V en aleatorio
-#separarlos en valores, bajo, medios y altos 
-# y mostrarlos en salida
+suma=0
+for i in range(1,10):
+    suma=suma+1/(i*(i+1))
+print(suma)
